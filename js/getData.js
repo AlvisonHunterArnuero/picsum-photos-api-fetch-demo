6459a4322474944422597de8a3d25c32a24e1269
Alvison Hunter <alvison.hunter@reeftechnology.com>
2021-09-30 23:17:30 -0600

    Adding first files to the remote repo

---
+++ js/getData.js
@@ -0,0 +1,25 @@
+const retrieveData = () => {
+  fetch("https://picsum.photos/v2/list?page=2&limit=99")
+    .then((response) => response.json())
+    .then((results) => displayPhotos(results));
+};
+
+const displayPhotos = (photoResults) => {
+  const photoContainer = document.querySelector(".unsplash");
+
+  let genHtmlContent ='<div class="container-fluid"><div class="row justify-content-md-center">';
+    photoResults.forEach(({ id, author, download_url, width }) => {
+        genHtmlContent += `
+            <div class="col-12 col-md-6 col-lg-4 my-3">
+            <div class="card">
+            <img src=https://picsum.photos/id/${id}/${width}/ loading="lazy"
+            class="card-img-top filtered">
+            <div class="card-body">
+            <h5 class="text-center author_name card-title">Photographer: ${author}</h5>
+            <a target="_blank" href=${download_url} class="btn btn-outline-dark col-12">Download Photo!</a>
+            </div></div></div>`;
+    }); 
+    
+genHtmlContent += "</div></div>";
+photoContainer.innerHTML = genHtmlContent;
+};
