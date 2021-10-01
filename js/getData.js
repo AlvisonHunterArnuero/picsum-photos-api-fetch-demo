88be5f874b3b103c75269638d5292df88f892f25
Alvison Hunter <alvison.hunter@reeftechnology.com>
2021-10-01 00:22:03 -0600

    Updated metatags and others

--- js/getData.js
+++ js/getData.js
@@ -7,19 +7,20 @@ const retrieveData = () => {
 const displayPhotos = (photoResults) => {
   const photoContainer = document.querySelector(".unsplash");
 
-  let genHtmlContent ='<div class="container-fluid"><div class="row justify-content-md-center">';
-    photoResults.forEach(({ id, author, download_url, width }) => {
-        genHtmlContent += `
+  let genHtmlContent =
+    '<div class="container-fluid w-75"><div class="row justify-content-md-center">';
+  photoResults.forEach(({ id, author, download_url, width }) => {
+    genHtmlContent += `
             <div class="col-12 col-md-6 col-lg-4 my-3">
-            <div class="card">
+            <div class="card align-items-center">
             <img src=https://picsum.photos/id/${id}/${width}/ loading="lazy"
             class="card-img-top filtered">
             <div class="card-body">
             <h5 class="text-center author_name card-title">Photographer: ${author}</h5>
-            <a target="_blank" href=${download_url} class="btn btn-outline-dark col-12">Download Photo!</a>
+            <a target="_blank" href=${download_url} class="btn btn-outline-dark w-100 mx-auto my-3">Download Photo!</a>
             </div></div></div>`;
-    }); 
-    
-genHtmlContent += "</div></div>";
-photoContainer.innerHTML = genHtmlContent;
+  });
+
+  genHtmlContent += "</div></div>";
+  photoContainer.innerHTML = genHtmlContent;
 };
