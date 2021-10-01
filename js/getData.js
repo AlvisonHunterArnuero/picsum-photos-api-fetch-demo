2a3d99b4f9e0b5f4e78a56268dd40afed668a38f
Alvison Hunter <alvison.hunter@reeftechnology.com>
2021-10-01 09:02:25 -0600

    Enhanced card. Added dimension. Moved author to the top. Added Font Awesome library.

--- js/getData.js
+++ js/getData.js
@@ -5,20 +5,34 @@ const retrieveData = () => {
 };
 
 const displayPhotos = (photoResults) => {
-  const photoContainer = document.querySelector(".unsplash");
+  const photoContainer = document.querySelector(".root");
 
-  let genHtmlContent =
-    '<div class="container-fluid w-75"><div class="row justify-content-md-center">';
-  photoResults.forEach(({ id, author, download_url, width }) => {
+  let genHtmlContent = '<div class="container-fluid w-75"><div class="row justify-content-md-center">';
+  photoResults.forEach(({ id, author, download_url, width, height }) => {
     genHtmlContent += `
-            <div class="col-12 col-md-6 col-lg-4 my-3">
-            <div class="card align-items-center">
-            <img src=https://picsum.photos/id/${id}/${width}/ loading="lazy"
-            class="card-img-top filtered">
-            <div class="card-body">
-            <h5 class="text-center author_name card-title">Photographer: ${author}</h5>
-            <a target="_blank" href=${download_url} class="btn btn-outline-dark w-100 mx-auto my-3">Download Photo!</a>
-            </div></div></div>`;
+    <div class="col-12 col-lg-6 col-xl-4 my-3">
+    <div class="card text-center">
+       <div class="author_name card-header">
+          <i class="fas fa-user"></i> Author: 
+          <span class="text-muted">${author}</span>
+       </div>
+       <img src=https://picsum.photos/id/${id}/${width}/ loading="lazy"
+          class="card-img-top filtered">
+       <div class="card-body">
+          <div class="card-title">
+             <ul class="list-group border-0">
+                <li class="list-group-item border-0">Id:${id}</li>
+                <li class="list-group-item border-0">Width:${width}px</li>
+                <li class="list-group-item border-0">Height:${height}px</li>
+             </ul>
+          </div>
+          <a target="_blank" href=${download_url} 
+             class="center btn btn-outline-dark my-3">
+          <i class="fas fa-arrow-circle-down"></i> 
+          Download Photo!</a>
+       </div>
+    </div>
+ </div>`;
   });
 
   genHtmlContent += "</div></div>";
